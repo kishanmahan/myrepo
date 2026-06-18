2cc3a4730750f11a914697dec2a6eaec320ef8b6
Kishan kumar <singhbhimrav@gmail.com>
2026-06-18 22:38:01 +0530

    changed uppercase text

--- Practicex.java
+++ Practicex.java
@@ -5,6 +5,6 @@ System.out.println("practice man be perfect");
 public static void main(String args[]){
 Practicex p=new Practicex();
 System.out.println("The website is added");
-System.out.println("kkr is doing well");
+System.out.println("kkr is good to Doing well");
 }}
 
