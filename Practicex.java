081ab4994b3059c3db7d414caea8d31064d0d3c9
Kishan kumar <singhbhimrav@gmail.com>
2026-06-18 22:12:17 +0530

    Changed

--- Practicex.java
+++ Practicex.java
@@ -4,7 +4,7 @@ System.out.println("practice man be perfect");
 }
 public static void main(String args[]){
 Practicex p=new Practicex();
-System.out.println("website is added");
+System.out.println("The website is added");
 System.out.println("kkr is doing well");
 }}
 
